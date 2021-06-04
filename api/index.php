a838826e9f489cbef56936b6a2c4f899ec51fe28
jeffersonm03 <jefferson.negociom03@gmail.com>
2021-06-04 16:59:33 -0300

    teste-php

--- api/index.php
+++ api/index.php
@@ -1,13 +1,4 @@
-<html>
-  <head>
-    <title>Teste</title>
-  </head>
-  <body>
-    <h2>
-      <?php
-          $bebida = "refrigerante";
-          print $bebida;
-      ?>
-    </h2>
-  </body>
-</html>
+<?php
+    $bebida = "refrigerante";
+    print $bebida;
+?>
